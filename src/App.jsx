b78a5fcb6db55c4a05cab9aac9f4f4ff65459782
shivank pundir <shivapundir1978@gmail.com>
2025-08-28 02:40:25 +0530

    fix app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
-import Navbar from './components/navbar'
-import HeroSecrion from './components/HeroSecrion'
-import FeatuerSection from './components/FeatuerSection'
+import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
+import FeatureSection from './components/FeatureSection'
 import WorkflowSection from './components/WorkflowSection'
 import Price from './components/Price'
-import Tasitimonials from './components/Tasitimonials'
+import Testimonials from './components/Testimonials'
 import Footer from './components/Footer'
+
 const App = () => {
   return (
     <div>
-    <Navbar />
-    <div className="max-w-7xl mx-auto pt-20 px-6">
-      <HeroSecrion />
-        <FeatuerSection />
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection />
         <WorkflowSection />
         <Price />
-        <Tasitimonials />
+        <Testimonials />
         <Footer />
-    </div>
-   
+      </div>
     </div>
   )
 }
